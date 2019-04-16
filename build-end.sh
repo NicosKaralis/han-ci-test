a78d38879f3b4ba3e31217d6fc1600f6f1c85a39
Nicos Karalis <nicoskaralis@me.com>
2019-04-16 13:11:56 -0300

    updated build script and report script
    Signed-off-by: Nicos Karalis <nicoskaralis@me.com>

--- build-end.sh
+++ build-end.sh
@@ -7,12 +7,12 @@ echo $2 # $TRAVIS_TEST_RESULT
 echo $3 # git rev-parse HEAD
 echo $4 # $TRAVIS_JOB_WEB_URL
 
-if [[ $2 eq '0' ]]; then
+if [ "$2" -eq "0" ]; then
   echo 'Igual a zero'
-  $STATUS='SUCCESSFUL'
+  STATUS='SUCCESSFUL'
 else
   echo 'Diferente de 0'
-  $STATUS='FAILED'
+  STATUS='FAILED'
 fi
 
 curl -X "POST" "https://api.bitbucket.org/2.0/repositories/anfeli/han-ios-app/commit/$3/statuses/build" \
